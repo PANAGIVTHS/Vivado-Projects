7771fb6a79067250d0fe3e127667e716167300c7
PANAGIVTHS <panagiotis.tsogka@gmail.com>
2024-10-15 23:24:57 +0300

    Remove Debounce reset

--- Lab1/debounce.v
+++ Lab1/debounce.v
@@ -4,27 +4,28 @@ module debounce(clk, reset, button, button_debounced);
     output reg button_debounced;
     reg [2:0] FF_wire;
     reg [6:0] counter;
+    reg enabled;
     wire button_ON;
 
-    always @(posedge clock or posedge reset) begin
-        if (reset) begin
-            FF_wire <= 3'b0;
-        end else begin
-            FF_wire[0] <= button;
-            FF_wire[1] <= FF_wire[0];
-            FF_wire[2] <= FF_wire[1];
-        end
+    // No need to reset cause button has only 2 states
+    // after 3 cycles, button_ON will be 0
+    always @(posedge clock) begin
+        FF_wire[0] <= button;
+        FF_wire[1] <= FF_wire[0];
+        FF_wire[2] <= FF_wire[1];
     end
 
     assign button_ON = FF_wire[1] & FF_wire[2];
+    assign enabled = ~(buttton_debounced & button_ON);
 
-    always @(posedge clk or posedge reset) begin
-        if (reset) begin
-            button_debounced <= 1'b0;
-            counter <= 8'b0;
-        end else begin
+    always @(posedge clk) begin
+        if (enabled) begin
             if (button_ON) begin 
                 {button_debounced, counter} <= counter + 1;
+            end
+            else begin
+                button_debounced <= 1'b0;
+                counter <= 8'b0;
             end
         end
     end
