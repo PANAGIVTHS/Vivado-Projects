3e56db07e127134794545ecd88a8058f04347071
PANAGIVTHS <panagiotis.tsogka@gmail.com>
2024-10-16 01:31:46 +0300

    Make Better LED_tb Add Char Decoder Memory Changed Characters In LED_decoder

--- Lab1/char_decoder.v
+++ Lab1/char_decoder.v
@@ -1,49 +1,48 @@
 
 
-module char_decoder (counter, char, button_presses);
+module char_decoder (counter, char, button_presses, reset);
     input [3:0] counter;
     output reg [3:0] char;
     input [4:0] button_presses;
-    reg [3:0] mem [4:0];
+    reg [4:0] mem [4:0];
     
+    always @(posedge reset) begin
+        mem[0] <= 4'b1100; // [
+        mem[1] <= 4'b0000; // S
+        mem[2] <= 4'b1110; // E
+        mem[3] <= 4'b0001; // G
+        mem[4] <= 4'b0010; // M
+        mem[5] <= 4'b1110; // E
+        mem[6] <= 4'b0011; // N
+        mem[7] <= 4'b0100; // T
+        mem[8] <= 4'b1010; // A
+        mem[9] <= 4'b0100; // T
+        mem[10] <= 4'b0101; // I
+        mem[11] <= 4'b0110; // O
+        mem[12] <= 4'b0011; // N
+        mem[13] <= 4'b1011; // -
+        mem[14] <= 4'b0111; // F
+        mem[15] <= 4'b1010; // A
+        mem[16] <= 4'b1000; // U
+        mem[17] <= 4'b1001; // L
+        mem[18] <= 4'b0100; // T
+        mem[19] <= 4'b1101; // ]
+    end
+
     always @(counter) begin
         case (counter)
-            AN0_SETUP_BUS: begin
-                char = mem[4'b0000 + button_presses];
-            end
-            4'b0001: begin
-                char = mem[4'b0000 + button_presses];
-            end
-            4'b0010: begin
-                char = mem[4'b0000 + button_presses];
-            end
-            AN1_SETUP_BUS: begin
-                char = mem[4'b0001 + button_presses];
-            end
-            4'b0100: begin
-                char = mem[4'b0001 + button_presses];
-            end
-            4'b0101: begin
-                char = mem[4'b0001 + button_presses];
-            end
-            AN2_SETUP_BUS: begin
-                char = mem[4'b0010 + button_presses];
-            end
-            4'b1000: begin
-                char = mem[4'b0010 + button_presses];
-            end
-            4'b1001: begin
-                char = mem[4'b0010 + button_presses];
-            end
-            AN3_SETUP_BUS: begin
-                char = mem[4'b0011 + button_presses];
-            end
-            4'b1110: begin
-                char = mem[4'b0011 + button_presses];
-            end
-            4'b1111: begin
-                char = mem[4'b0011 + button_presses];
-            end
+            AN0_SETUP_BUS: char = mem[4'b0000 + button_presses];
+            4'b0001: char = mem[4'b0000 + button_presses];
+            4'b0010: char = mem[4'b0000 + button_presses];
+            AN1_SETUP_BUS: char = mem[4'b0001 + button_presses];
+            4'b0100: char = mem[4'b0001 + button_presses];
+            4'b0101: char = mem[4'b0001 + button_presses];
+            AN2_SETUP_BUS: char = mem[4'b0010 + button_presses];
+            4'b1000: char = mem[4'b0010 + button_presses];
+            4'b1001: char = mem[4'b0010 + button_presses];
+            AN3_SETUP_BUS: char = mem[4'b0011 + button_presses];
+            4'b1110: char = mem[4'b0011 + button_presses];
+            4'b1111: char = mem[4'b0011 + button_presses];
         endcase
     end
 endmodule
--- Lab1/testbench.v
+++ Lab1/testbench.v
@@ -2,26 +2,34 @@
 module tb_;
     reg [3:0] char;
     wire [6:0] LED;
-
+    
+    // Task to display the 7-segment representation visually
+    task display_segment;
+        input [3:0] char;
+        input [6:0] LED;
+        begin
+            // Print the visual representation of the 7-segment display
+            $display(" %s ", LED[6] ? "     " : "----");
+            
+            $display("%s   %s", LED[1] ? " " : "|", LED[5] ? " " : "   |");
+            $display("%s   %s", LED[1] ? " " : "|", LED[5] ? " " : "   |");
+            
+            $display(" %s ", LED[0] ? "     " : "----");
+            
+            $display("%s   %s", LED[2] ? " " : "|", LED[4] ? " " : "   |");
+            $display("%s   %s", LED[2] ? " " : "|", LED[4] ? " " : "   |");
+            
+            $display(" %s ", LED[3] ? "     " : "----");
+            $display("\n");
+        end
+    endtask
     LEDdecoder LEDdecoder_ (.char(char), .LED(LED));
 
     initial begin
-        char = 4'b0000;
-        #10 char = 4'b0001;
-        #10 char = 4'b0010;
-        #10 char = 4'b0011;
-        #10 char = 4'b0100;
-        #10 char = 4'b0101;
-        #10 char = 4'b0110;
-        #10 char = 4'b0111;
-        #10 char = 4'b1000;
-        #10 char = 4'b1001;
-        #10 char = 4'b1010;
-        #10 char = 4'b1011;
-        #10 char = 4'b1100;
-        #10 char = 4'b1101;
-        #10 char = 4'b1110;
-        #10 char = 4'b1111;
-        #10 $finish;
+        // Loop through all possible characters
+        for (char = 4'b0000; char <= 4'b1111; char = char + 1) begin
+            #10 display_segment(char, LED);
+        end
+        $finish; // Stop the simulation
     end
 endmodule
