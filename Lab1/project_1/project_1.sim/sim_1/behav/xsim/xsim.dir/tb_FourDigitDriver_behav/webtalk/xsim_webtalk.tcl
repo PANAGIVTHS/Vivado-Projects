9d0903616973aa2f6a9499dc59aeb2a73a202347
PANAGIVTHS <panagiotis.tsogka@gmail.com>
2024-10-16 12:53:41 +0300

    Nothing to ccommit just Vivado things

--- Lab1/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/tb_FourDigitDriver_behav/webtalk/xsim_webtalk.tcl
+++ Lab1/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/tb_FourDigitDriver_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/panag/Desktop/GitHub/Repositories/Vivado-Code/Lab1/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/tb_FourDigitDriver_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Wed Oct 16 04:39:09 2024" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Wed Oct 16 04:55:40 2024" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.3 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2405991" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "e567dcc3-3148-486f-a856-9ad9a00717dc" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "34f4a2b7988b4b7b8f8a61a08bec529e" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "118" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "119" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "AMD Ryzen 7 5800X 8-Core Processor             " -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "2" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.00_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "8576_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 2956311474 -regid "" -xml C:/Users/panag/Desktop/GitHub/Repositories/Vivado-Code/Lab1/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/tb_FourDigitDriver_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/panag/Desktop/GitHub/Repositories/Vivado-Code/Lab1/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/tb_FourDigitDriver_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/panag/Desktop/GitHub/Repositories/Vivado-Code/Lab1/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/tb_FourDigitDriver_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.01_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "8548_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 976067512 -regid "" -xml C:/Users/panag/Desktop/GitHub/Repositories/Vivado-Code/Lab1/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/tb_FourDigitDriver_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/panag/Desktop/GitHub/Repositories/Vivado-Code/Lab1/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/tb_FourDigitDriver_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/panag/Desktop/GitHub/Repositories/Vivado-Code/Lab1/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/tb_FourDigitDriver_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
