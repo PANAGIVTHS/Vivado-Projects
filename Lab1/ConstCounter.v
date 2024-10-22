a55951d7586a65ee0b739503e7668231d86c2776
PANAGIVTHS <panagiotis.tsogka@gmail.com>
2024-10-22 22:14:21 +0300

    Fix some stuff

--- Lab1/ConstCounter.v
+++ Lab1/ConstCounter.v
@@ -1,9 +1,9 @@
 module ConstCounter (input clk, input reset, output reg [3:0] counter);
     always @(posedge clk or posedge reset) begin
         if (reset == 1'b1) begin 
-            counter = 4'b0000;
+            counter <= 4'b0000;
         end else begin
-            counter = counter + 4'b0001;
+            counter <= counter + 4'b0001;
         end
     end
 endmodule
