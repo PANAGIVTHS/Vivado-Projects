a15500cd92aafd775f97214e9d518d9e5968ae4b
PANAGIVTHS <panagiotis.tsogka@gmail.com>
2024-10-30 18:55:54 +0200

    Refactor code a bit Test successful

--- Lab2/UART_controller.v
+++ Lab2/UART_controller.v
@@ -1,7 +1,7 @@
 module UART_controller (reset, clk, baud_select, Tx_DATA, Tx_WR, Tx_EN, TxD, Tx_BUSY, Tx_DATA_copy);
     input reset, clk;
     input [7:0] Tx_DATA;
-    output reg [0:9] Tx_DATA_copy;
+    output reg [0:10] Tx_DATA_copy;
     input wire [2:0] baud_select;
     input Tx_WR, Tx_EN;
     output wire TxD;
@@ -9,29 +9,28 @@ module UART_controller (reset, clk, baud_select, Tx_DATA, Tx_WR, Tx_EN, TxD, Tx_
     wire sample_ENABLE;
     reg [3:0] counter;
 
+    // Instantiate baud rate controller for transmitter to enable sampling at the required frequency
     baud_controller_t baud_controller_t_inst (.reset(reset), .clk(clk), .baud_select(baud_select), .sample_ENABLE(sample_ENABLE));
     uart_transmitter uart_transmitter_inst (.reset(reset), .clk(clk), .Tx_DATA(Tx_DATA), .baud_select(baud_select), .Tx_WR(Tx_WR), .Tx_EN(Tx_EN), .TxD(TxD), .Tx_BUSY(Tx_BUSY));
 
+    localparam FRAME_END = 4'b1010;
+
+    // Counter for buffer
     always @(posedge clk or posedge reset) begin
         if (reset) begin
-            // This is not a bug with the modules just need to wait 1 cycle for the value to change after the enable
-            // We do this cancerous thing because it's 4am and my C brain go brrrr
             counter <= 0;
         end
     end
 
+    // Fill buffer with data from transmission
     always @(sample_ENABLE) begin
         if (Tx_EN && Tx_BUSY && sample_ENABLE) begin
-            if (counter != -1) begin
             Tx_DATA_copy[counter] <= TxD;
-                    // Possible bug:
-                    end 
-            if (counter == 4'b1001) begin
+            if (counter == FRAME_END) begin
                 counter <= 0;
             end else begin
                 counter <= counter + 1;
             end
         end
-
     end
 endmodule
--- Lab2/uart_transmitter.v
+++ Lab2/uart_transmitter.v
@@ -9,17 +9,21 @@ module uart_transmitter (
     output reg TxD,              // Output serial data
     output reg Tx_BUSY           // Transmission status
 );
-    
-    // Shift register bits (0-10 για τα 11 bit)
-    wire sample_ENABLE;           // Signal to sample data
+    wire sample_ENABLE;
     reg [3:0] cur_state, next_state;
+    
+    // State machine states
+    // All from START_BIT to DISABLED are used for the state machine
+    localparam START_BIT = 4'b0000, PARITY = 4'b1010, IDLE = 4'b1011, DISABLED = 4'b1100;
+
+    // Buffer to store data to be transmitted
     wire [0:9] buffer = {1'b0, Tx_DATA, 1'b1}; // Add start and stop bits to data
 
-    localparam START_BIT = 4'b0000, IDLE = 4'b1010, DISABLED = 4'b1011, UNUSED1 = 4'b1101, UNUSED2 = 4'b1110, UNUSED3 = 4'b1111;
-
+    // Instantiate baud rate controller for transmitter to enable sampling at the required frequency
     baud_controller_t baud_controller_t_inst (.reset(reset), .clk(clk), .baud_select(baud_select), .sample_ENABLE(sample_ENABLE));
 
-    always @(posedge clk or posedge reset) begin 
+    // State machine state register
+    always @(posedge clk or posedge reset) begin
         if (reset) begin
             next_state <= IDLE;
         end else begin
@@ -27,7 +31,8 @@ module uart_transmitter (
         end
     end
 
-    always @(Tx_EN or Tx_WR or sample_ENABLE) begin 
+    // State machine logic
+    always @(Tx_EN or Tx_WR or sample_ENABLE) begin
         if (!Tx_EN) begin
             next_state <= DISABLED; // Start-SetUp transmission
         end else if (!Tx_WR && !Tx_BUSY) begin
@@ -41,6 +46,7 @@ module uart_transmitter (
         end
     end
 
+    // State machine output
     always @(cur_state) begin
         case(cur_state)
             DISABLED: begin
@@ -50,6 +56,10 @@ module uart_transmitter (
             IDLE: begin
                 Tx_BUSY <= 0; // Set state to idle
                 TxD <= 1; // No transmission
+            end
+            PARITY: begin
+                Tx_BUSY <= 1; // Set state to busy
+                TxD <= ^Tx_DATA; // Parity bit
             end
             // TRANSMITTING
             default: begin
